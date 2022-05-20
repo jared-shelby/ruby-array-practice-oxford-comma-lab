701dd735bd77d7c41cee9b5d41f3a1a6fe95ebe5
Jared Shelby <jared.shelby@yale.edu>
2022-05-20 14:00:46 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    arr_size = array.size
+    if arr_size <= 1
+        return array.join
+    elsif arr_size == 2
+        return array.first + " and " + array.last
+    else
+        array[-1] = "and " + array.last
+        array.join(", ")
+    end
 end
